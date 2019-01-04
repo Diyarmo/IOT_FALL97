d75e536a940d4b0c0df8777f022930ff3d894fbf
unknown <behzad.shayegh@ut.ac.ir>
2019-01-04 09:07:06 +0330

    final

--- public/javascripts/sign in.js
+++ public/javascripts/sign in.js
@@ -1,40 +1,23 @@
-var group = document.querySelector('.js-groupname');
-var pass = document.querySelector('.js-password');
-var btn = document.querySelector('.js-button');
+var btn = document.querySelector('.Log');
 
-this_pass = "setare";
-
-function Log_In(){
-	if(group.value == "" || pass.value == "")
-		return alertify.notify("groupname or password is empty!", 'error', 3);
-	else if(this_pass==null || this_pass!= pass.value)
-		return alertify.notify("incorrect groupname or password!", 'error', 3);
-	else{
-		alertify.notify("your sign in is successfully done!", 'success', 3);
-		window.open("../htmlfiles/dashboard.html","_self");
-	}
+function Log_In() {
+    var user = document.querySelector('.js-groupname').value;
+    var pass = document.querySelector('.js-password').value;
+    if (user == '' | pass == '')
+    {
+        alertify.notify("Fill all inputs",'error',3);
+        return;
+    }
+    var request = new XMLHttpRequest();
+    request.open('POST', '/login');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({
+        groupname: user,
+        password: pass
+    }))
+    request.addEventListener('loadend', function(){
+        var f = JSON.parse(request.response);
+        alertify.notify(f.message, 'error', 3);
+       if(f.go == true) window.open("../htmlfiles/dashboard.html","_self");
+    })
 }
-
-
-
-
-
-
-
-// dashboard
-	document.getElementById("nemodar1").setAttribute("src","https://thingspeak.com/channels/667501/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15");
-	document.getElementById("nemodar2").setAttribute("src","https://thingspeak.com/channels/667501/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15");
-	document.getElementById("nemodar3").setAttribute("src","https://thingspeak.com/channels/667501/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15");
-	document.getElementById("nemodar4").setAttribute("src","https://thingspeak.com/channels/667501/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15");
-	document.getElementById("js-username").innerHTML = "X-mens"
-
-	document.getElementById("user1").innerHTML = "users1"
-	document.getElementById("user2").innerHTML = "users2"
-	document.getElementById("user3").innerHTML = "users3"
-	document.getElementById("user4").innerHTML = "users4"
-	document.getElementById("user5").innerHTML = "users5"
-	document.getElementById("user6").innerHTML = "users6"
-	document.getElementById("user7").innerHTML = "users7"
-	document.getElementById("user8").innerHTML = "users8"
-	document.getElementById("user9").innerHTML = "users9"
-	document.getElementById("user10").innerHTML = "users10"
--- public/javascripts/sign up.js
+++ public/javascripts/sign up.js
@@ -1,22 +1,34 @@
-var group = document.querySelector('.js-groupname');
-var pass = document.querySelector('.js-password');
-var re_pass = document.querySelector('.js-re-password');
-
-
-var btn = document.querySelector('.js-button');
+var btn = document.querySelector('.Reg');
+var users = new Array(10);
 
 function Sign_Up() {
-	if (group.value == "" || pass.value == "" || re_pass.value == "") {
-		return alertify.notify("groupname or password is empty!", 'error', 3);
+	var user = document.querySelector('.js-groupname').value;
+	var chid = document.querySelector('.js-channelid').value;
+	var pass = document.querySelector('.js-password').value;
+	var pass2 = document.querySelector('.js-re-password').value;
+    if (user == '' | pass == '' | chid == '')
+    {
+        return alertify.notify("groupname or password or channel id is empty!", 'error', 3);
 	}
-	else if (pass.value != re_pass.value) {
-		return alertify.notify("incorrect password!", 'error', 3);
-	}
+	if (pass != pass2) return alertify.notify("incorrect passwords!", 'error', 3);
+	
+    var request = new XMLHttpRequest();
+    request.open('POST', '/signup');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({
+        groupname: user,
+		password: pass,
+		channelID: chid,
+        members: users
+    }));
+    request.addEventListener('loadend', function(){
+        var f = JSON.parse(request.response);
+        alertify.notify(f.message);
+    });
+
 }
 
-var i = 1;
-
-var users = new Array(10);
+var i=0;
 
 function textBoxCreate() {
 	if(i > 9) return alertify.notify("many users!", 'error', 3);
@@ -25,12 +37,17 @@ function textBoxCreate() {
 	y.setAttribute("Placeholder", "User" + i);
 	y.setAttribute("Name", "Name_" + i);
 	document.getElementById("myForm").appendChild(y);
-	y.style.width = "130px";
+	y.style.width = "150px";
 	y.style.height = "24px";
 	y.style.backgroundColor = "rgb(248, 251, 255)";
 	y.style.borderColor = "darkgray";
 	y.style.color = "darkgray";
-	users[i] = y;
+	y.style.position = "relative";
+	y.style.top = "20px";
+	y.style.left = "70px";
+	
 	document.getElementById("myMain").style.height = "120%";
+	
+	users[i] = y;
 	i++;
 }
